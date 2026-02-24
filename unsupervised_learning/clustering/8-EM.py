fc2ea71b92777969bdeba54d6fd20ecf7fd0c230
Stecie <156072665+Stecie06@users.noreply.github.com>
2026-02-24 16:17:21 +0200

    Update 8-EM.py

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-"""Calculates the EM"""
+"""[summary]
+
+Returns:
+    [type]: [description]
+"""
 import numpy as np
 initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
@@ -7,57 +11,44 @@ maximization = __import__('7-maximization').maximization
 
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
-    """
-    Performs the expectation maximization for a GMM
+    """[summary]
+
     Args:
-        X: is a numpy.ndarray of shape (n, d) containing the data set
-        k: is a positive integer containing the number of clusters
-        iterations: is a positive integer containing the maximum number
-        of iterations for the algorithm
-        tol: is a non-negative float containing tolerance of the log
-        likelihood, used to determine early stopping i.e. if the difference
-        is less than or equal to tol you should stop the algorithm
-        verbose: is a boolean that determines if you should print information
-        about the algorithm
+        X ([type]): [description]
+        k ([type]): [description]
+        iterations (int, optional): [description]. Defaults to 1000.
+        tol ([type], optional): [description]. Defaults to 1e-5.
+        verbose (bool, optional): [description]. Defaults to False.
 
-    Returns: pi, m, S, g, l, or None, None, None, None, None on failure
-    - pi is numpy.ndarray of shape (k,) containing the priors for each cluster
-    - m is a numpy.ndarray of shape (k, d) containing the centroid means
-    for each cluster
-    - S is a numpy.ndarray of shape (k, d, d) containing the covariance
-    matrices for each cluster
-    - g is a numpy.ndarray of shape (k, n) containing the probabilities
-    for each data point in each cluster
-    - log is the log likelihood of the model
+    Returns:
+        [type]: [description]
     """
-    if type(X) is not np.ndarray or len(X.shape) != 2:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None, None
-    if type(k) is not int or k <= 0:
+    if type(k) != int or k <= 0 or k >= X.shape[0]:
         return None, None, None, None, None
-    if type(iterations) is not int or iterations <= 0:
+    if type(iterations) != int or iterations <= 0:
         return None, None, None, None, None
-    if type(tol) is not float or tol < 0:
+    if type(tol) != float or tol <= 0:
         return None, None, None, None, None
-    if type(verbose) is not bool:
+    if type(verbose) != bool:
         return None, None, None, None, None
-
-    l_prev = 0
     pi, m, S = initialize(X, k)
-    g, log = expectation(X, pi, m, S)
-
+    prev_like = 0
+    g, likelihood = expectation(X, pi, m, S)
     for i in range(iterations):
         if verbose and (i % 10 == 0):
-            print('Log Likelihood after {} iterations: {}'.
-                  format(i, log.round(5)))
-
+            msg = 'Log Likelihood after {} iterations: {}'\
+                .format(i, likelihood.round(5))
+            print(msg)
         pi, m, S = maximization(X, g)
-        g, log = expectation(X, pi, m, S)
+        g, likelihood = expectation(X, pi, m, S)
 
-        if abs(l_prev - log) <= tol:
+        if abs(likelihood - prev_like) <= tol:
             break
-        l_prev = log
-
+        prev_like = likelihood
     if verbose:
-        print('Log Likelihood after {} iterations: {}'
-              .format(i + 1, log.round(5)))
-    return pi, m, S, g, log
+        msg = 'Log Likelihood after {} iterations: {}'\
+            .format(i + 1, likelihood.round(5))
+        print(msg)
+    return pi, m, S, g, likelihood
